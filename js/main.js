c29c5c795517a070af0be3ca869e6aabd5e20259
Carlos G. Fernandez <ionlotus1993@gmail.com>
2023-11-12 17:10:31 -0500

    Market cap Per Coin

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var mykey = config.MY_KEY;
 
 async function fetchData(timeFrame, selectedCoin) {
     const url = `https://coinranking1.p.rapidapi.com/coin/${selectedCoin}/history?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=${timeFrame}`;
+    const supply_url = `https://coinranking1.p.rapidapi.com/coin/${selectedCoin}/supply`;
+    const price_url = `https://coinranking1.p.rapidapi.com/coin/${selectedCoin}/price?referenceCurrencyUuid=yhjMzLPhuIDl`;
     const options = {
         method: 'GET',
         headers: {
@@ -12,30 +14,45 @@ async function fetchData(timeFrame, selectedCoin) {
     };
 
     try {
+        // Fetch Calls
         const response = await fetch(url, options);
+        const supplyCall = await fetch(supply_url, options);
+        const priceCall = await fetch(price_url, options);
+
+        // Check if any of the fetch calls failed
+        if (!response.ok || !supplyCall.ok || !priceCall.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
         const jsonResult = await response.json();
+        const jsonSupply = await supplyCall.json();
+        const jsonPrice = await priceCall.json();
 
         // Extract data from the API response
         const data = jsonResult.data.history;
+        const supplyData = jsonSupply.data.supply;
+        const priceData = jsonPrice.data.price;
 
-        // Extract timestamps and prices
+        // Extract timestamps, prices, supply
         const timestamps = data.map(entry => entry.timestamp);
         const prices = data.map(entry => entry.price);
+        const supply = supplyData['circulatingAmount']
+
 
         // Use timestamps to generate human-readable dates
         const dates = generateDates(timestamps, timeFrame);
-
 
         // Destroy the existing chart if it exists
         if (myChart) {
             myChart.destroy();
         }
 
-        // Continue with chart creation, including coin information
+        // Continue with chart creation, including coin information and last known price
         createChart(dates.reverse(), prices.reverse(), selectedCoin);
+        barChart(priceData, supply, selectedCoin);
 
     } catch (error) {
-        console.error(error);
+        console.error(error.message);
     }
 }
 
@@ -140,6 +157,64 @@ function createChart(labels, data, selectedCoin) {
         }
     });
 }
+
+let marketCapChart; // Declare marketCapChart outside functions for later reference
+
+function barChart(priceData, supply, selectedCoin) {
+    // Calculate market cap
+    const marketCap = priceData * supply;
+
+    // Check if marketCapChart exists
+    if (!marketCapChart) {
+        // Create the bar chart
+        const ctx = document.getElementById('marketCapChart').getContext('2d');
+        marketCapChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [nameSelection(selectedCoin)], // Use an array for labels
+                datasets: [{
+                    label: 'Market Cap (USD)',
+                    data: [marketCap], // Use an array for data
+                    borderColor: 'rgba(255, 215, 0, 0.5)',
+                    backgroundColor: 'rgba(255, 215, 0, 0.5)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        // Include a dollar sign in the ticks
+                        ticks: {
+                            callback: function (value, index, values) {
+                                return '$' + value.toFixed(2);
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false // Hide the legend
+                    }
+                }
+            }
+        });
+    } else {
+        // Update the existing chart
+        marketCapChart.data.labels = [nameSelection(selectedCoin)];
+        marketCapChart.data.datasets[0].data = [marketCap];
+
+        // Update border color and background color
+        marketCapChart.data.datasets[0].backgroundColor = colorSelection(selectedCoin); // Update to your desired background color
+        marketCapChart.data.datasets[0].borderColor = colorSelection(selectedCoin); // Update to your desired border color
+
+        marketCapChart.update();
+    }
+
+}
+
+
+
 
 function updateChart() {
     const selectedCoin = document.getElementById('coin').value;
